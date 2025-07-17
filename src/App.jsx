23d91875a029ebb64a1a9cc51a1e9662da5e9d47
WonyoungJung <yesdino06@gmail.com>
2025-07-17 18:44:18 +0900

    뼈대 수정본

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+// App.jsx
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyle from './styles/GlobalStyle';
+
+import Header from './components/Header'; // 헤더 추가
+import Footer from './components/Footer';
+
+import MainPage from './pages/MainPage';
+import DiaryListPage from './pages/DiaryListPage';
+import DiaryWritingPage from './pages/DiaryWritingPage';
+import LoginPage from './pages/LoginPage';
+import MyInformationPage from './pages/MyInformationPage';
+import MovePage from './pages/MovePage';
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <GlobalStyle/>
+      <Header />
+      <div style={{ paddingTop: '60px' }}> {/* Header 높이만큼 여백 추가 */}
+        <Routes>
+          <Route path="/" element={<MovePage />} />
+          <Route path="/MainPage" element={<MainPage />} />
+          <Route path="/DiaryListPage" element={<DiaryListPage />} />
+          <Route path="/DiaryWritingPage" element={<DiaryWritingPage />} />
+          <Route path="/LoginPage" element={<LoginPage />} />
+          <Route path="/MyInformationPage" element={<MyInformationPage />} />
+
+        </Routes>
+      </div>
+      <Footer/>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,19 @@
+//  로그인  
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const LoginPage =()=>{
+    const navigate = useNavigate();
+    return(
+        <>   
+            
+            <div >
+                <p>LoginPage</p>
+            </div>
+            
+        </>
+    );
+};
+
+export default LoginPage;
